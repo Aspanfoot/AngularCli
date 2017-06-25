4a7e029f90765aed967ebb156b11fb19ec83a5c8
Roman Vasuliv <superhappydinosaur@gmail.com>
2017-06-25 17:02:59 +0300

    List show/hide on checkbox value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  checkbox: boolean = false
+  objects: myObject[] = []
+
+  constructor() {
+    for (let i=1; i<5; i++) {
+      this.objects.push(new myObject(i,'Name' + i.toString(),i*10))
+    }
+  }
+
+  public checkBoxChangeListener(event) {
+    console.log(this.checkbox);
+  }
+}
+
+export class myObject {
+  id: number;
+  name: string;
+  dencity: number;
+
+  constructor(id:number, name:string, dencity:number) {
+    this.id = id;
+    this.name = name;
+    this.dencity = dencity;
+  }
 }
